486bfa8eb1799fa4427e647669b65fbe1f271f80
unknown <acharlakshmiv66@gmail.com>
2026-02-19 17:59:59 +0530

    Updated_URL_frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vehicle-rental-backend-sandy.vercel.app/api",
 });
 
 // Automatically attach JWT token to every request
