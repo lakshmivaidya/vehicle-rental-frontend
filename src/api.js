de23383f37a99953eec0b9c135a4104d29a87bda
unknown <acharlakshmiv66@gmail.com>
2026-04-22 17:46:30 +0530

    added_deployed_URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vehicle-rental-backend-mu.vercel.app/api",
 });
 
 // Attach token automatically for every request
